520358e2a1884884a8a8ea0f2b7cfe25415864a6
kweon <anybil30@gmail.com>
2026-02-12 17:31:39 +0900

    시간수정

--- src/pages/nanum/Nanum.js
+++ src/pages/nanum/Nanum.js
@@ -32,26 +32,16 @@ const Nanum = () => {
       fetchData();
     },[filter]);
 
+    
     // 타이머 설정
-    const getRemainingTime = (endTime) => {
-      if (!endTime) return "";
-
-      const utcTime = new Date(endTime);
-
-      // KST 보정 (+9시간)
-      const kstTime = new Date(utcTime.getTime() + (9 * 60 * 60 * 1000));
-
-      const now = new Date();
-      const remain = kstTime - now;
-
+    const getRemainingTime = (endTime) =>{
+      const remain = new Date(endTime) - new Date();
       if (remain <= 0) return "종료됨";
 
-      const hours = Math.floor(remain / (1000 * 60 * 60));
-      const minutes = Math.floor((remain % (1000 * 60 * 60)) / (1000 * 60));
-
+      const hours = Math.floor(remain / (1000*60*60));
+      const minutes = Math.floor((remain % (1000*60*60)) / (1000*60));
       return `${hours}시간 ${minutes}분 남음`;
-    };
-
+    }
 
   return (
     <main>
